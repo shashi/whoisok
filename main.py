bda45ddf37b7bf9a51ffe19a219079c1a67b375b
Shashi Gowda <connect2shashi@gmail.com>
2010-12-04 13:18:19 +0530

    Staging of stolen code

---
+++ main.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+#
+# main.py
+#
+
+import cgi
+import datetime
+import wsgiref.handlers
+
+from google.appengine.ext import db
+from google.appengine.api import users
+from google.appengine.ext import webapp
+
+class Greeting(db.Model):
+  author = db.UserProperty()
+  content = db.StringProperty(multiline=True)
+  date = db.DateTimeProperty(auto_now_add=True)
+
+
+class MainPage(webapp.RequestHandler):
+  def get(self):
+    self.response.out.write('<html><body>')
+
+    greetings = db.GqlQuery("SELECT * "
+                            "FROM Greeting "
+                            "ORDER BY date DESC LIMIT 10")
+
+    for greeting in greetings:
+      if greeting.author:
+        self.response.out.write('<b>%s</b> wrote:' % greeting.author.nickname())
+      else:
+        self.response.out.write('An anonymous person wrote:')
+      self.response.out.write('<blockquote>%s</blockquote>' %
+                              cgi.escape(greeting.content))
+
+    self.response.out.write("""
+          <form action="/sign" method="post">
+            <div><textarea name="content" rows="3" cols="60"></textarea></div>
+            <div><input type="submit" value="Sign Guestbook"></div>
+          </form>
+        </body>
+      </html>""")
+  def start_session(self):
+    session = appengine_utilities.sessions.Session()
+    session[""] = "value"
+
+class Guestbook(webapp.RequestHandler):
+  def post(self):
+    greeting = Greeting()
+
+    if users.get_current_user():
+      greeting.author = users.get_current_user()
+
+    greeting.content = self.request.get('content')
+    greeting.put()
+    self.redirect('/')
+
+application = webapp.WSGIApplication([
+  ('/', MainPage),
+  ('/sign', Guestbook)
+], debug=True)
+
+
+def main():
+  wsgiref.handlers.CGIHandler().run(application)
+
+
+if __name__ == '__main__':
+  main()
